41c7991770c975fc559f2ff27e756d8864900136
Noa Cohen <noac@moveo.co.il>
2022-03-16 14:07:08 +0200

    Implement Splash

---
+++ app/src/main/java/com/noalecohen/dispatcher/StartupActivity.java
@@ -0,0 +1,32 @@
+package com.noalecohen.dispatcher;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class StartupActivity extends AppCompatActivity {
+
+    Handler handler;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_startup);
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        fragmentManager.beginTransaction().replace(R.id.startup_frame_content, new SplashFragment()).commit();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        handler = new Handler();
+        handler.postDelayed(() -> {
+            Intent intent = new Intent(this, MainActivity.class);
+            startActivity(intent);
+            finish();
+        }, 3000);
+    }
+}
