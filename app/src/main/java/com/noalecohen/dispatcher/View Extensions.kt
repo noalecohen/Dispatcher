eff90b9e02257489794b4247c190da52907459a9
Noa Cohen <noac@moveo.co.il>
2022-03-20 16:57:17 +0200

    Implement view extensions

---
+++ app/src/main/java/com/noalecohen/dispatcher/View Extensions.kt
@@ -0,0 +1,16 @@
+package com.noalecohen.dispatcher
+
+import android.view.View
+import android.widget.Toast
+import model.BaseFragment
+
+fun BaseFragment.displayToast(str: String) = Toast.makeText(context, str, Toast.LENGTH_LONG).show()
+
+fun View.showView(boolean: Boolean): View {
+    visibility = if(boolean){
+        View.VISIBLE
+    }else {
+        View.INVISIBLE
+    }
+    return this
+}
