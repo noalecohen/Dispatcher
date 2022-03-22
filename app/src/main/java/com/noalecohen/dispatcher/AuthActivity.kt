9ce9c8db9a83c7b6319e53d0289678edcb8937af
Noa Cohen <noac@moveo.co.il>
2022-03-22 21:12:45 +0200

    Design loginFragment and registerFragment

--- app/src/main/java/com/noalecohen/dispatcher/AuthActivity.kt
+++ app/src/main/java/com/noalecohen/dispatcher/AuthActivity.kt
@@ -1,14 +1,25 @@
 package com.noalecohen.dispatcher
 
 import android.os.Bundle
+import android.util.Log
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import com.noalecohen.dispatcher.databinding.ActivityAuthBinding
 
 class AuthActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityAuthBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_auth)
+        binding = ActivityAuthBinding.inflate(layoutInflater)
+    }
+
+    override fun onStart() {
+        super.onStart()
         val fragmentManager = supportFragmentManager
-        fragmentManager.beginTransaction().replace(R.id.startup_frame_content, SplashFragment())
+        fragmentManager.beginTransaction().replace(R.id.startup_frame_content, LoginFragment())
             .commit()
     }
+
 }
