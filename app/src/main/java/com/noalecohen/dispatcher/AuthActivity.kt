95eeb981d5781cd2f2020fc8f3df6c963131bede
Noa Cohen <noac@moveo.co.il>
2022-03-22 13:25:54 +0200

    Init Auth design

--- app/src/main/java/com/noalecohen/dispatcher/AuthActivity.kt
+++ app/src/main/java/com/noalecohen/dispatcher/AuthActivity.kt
@@ -3,10 +3,10 @@ package com.noalecohen.dispatcher
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
-class LogisterActivity : AppCompatActivity() {
+class AuthActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_logister)
+        setContentView(R.layout.activity_auth)
         val fragmentManager = supportFragmentManager
         fragmentManager.beginTransaction().replace(R.id.startup_frame_content, SplashFragment())
             .commit()
