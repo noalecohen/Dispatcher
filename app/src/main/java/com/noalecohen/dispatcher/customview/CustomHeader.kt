628fc22c3f5ca595a7f67e0093bba94e6cd5023c
Noa Cohen <noac@moveo.co.il>
2022-03-27 17:50:52 +0300

    Fix syntax

--- app/src/main/java/com/noalecohen/dispatcher/customview/CustomHeader.kt
+++ app/src/main/java/com/noalecohen/dispatcher/customview/CustomHeader.kt
@@ -30,7 +30,7 @@ class CustomHeader(context: Context, attrs: AttributeSet?) : ConstraintLayout(co
     }
 
     fun setShowIcon(icon: View, toShow: Boolean) {
-        if (toShow) icon.visibility = View.VISIBLE else icon.visibility = View.GONE
+        icon.visibility = if(toShow) View.VISIBLE else View.GONE
     }
 
     fun setIconClickListener(icon: ImageButton, callBack: OnClickListener) {
