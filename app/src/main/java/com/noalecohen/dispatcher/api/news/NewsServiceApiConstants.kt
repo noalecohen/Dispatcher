c5a612a5b0694379987d4aa01605a26fac94a1e3
Noa Cohen <noac@moveo.co.il>
2022-03-29 09:58:14 +0300

    API KEY

--- app/src/main/java/com/noalecohen/dispatcher/api/news/NewsServiceApiConstants.kt
+++ app/src/main/java/com/noalecohen/dispatcher/api/news/NewsServiceApiConstants.kt
@@ -2,5 +2,4 @@ package com.noalecohen.dispatcher.api.news
 
 object NewsServiceApiConstants {
     const val TOP_HEADLINES_ENDPOINT = "top-headlines"
-    const val API_KEY = "16eae76025944d5ab86e8f40d9b94db1"
 }
--- app/src/main/java/com/noalecohen/dispatcher/api/news/NewsServiceApiController.kt
+++ app/src/main/java/com/noalecohen/dispatcher/api/news/NewsServiceApiController.kt
@@ -1,5 +1,6 @@
 package com.noalecohen.dispatcher.api.news
 
+import com.noalecohen.dispatcher.BuildConfig
 import com.noalecohen.dispatcher.api.ApiController
 import com.noalecohen.dispatcher.model.response.News
 import retrofit2.Call
@@ -7,7 +8,7 @@ import retrofit2.Call
 class NewsServiceApiController() : ApiController() {
 
     fun fetchTopHeadlinesByCountry(country: String): Call<News> {
-        return api!!.fetchTopHeadlinesByCountry(country, NewsServiceApiConstants.API_KEY)
+        return api!!.fetchTopHeadlinesByCountry(country, BuildConfig.NEWS_API_KEY)
     }
 
 }
